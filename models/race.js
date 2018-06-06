25252757ce91fcca267362acdad09a34cd987b8d
JakaCugalj <33024213+JakaCugalj@users.noreply.github.com>
2018-06-06 15:52:44 +0200

    Initial commit

---
+++ models/race.js
@@ -0,0 +1,23 @@
+//shema za objekt proga
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var t = require('./track.js');
+var TrackSchema = mongoose.model('track').schema;
+var d = require('./driver.js');
+var DriverSchema = mongoose.model('driver').schema;
+var v = require('./vehicle.js');
+var VehicleSchema = mongoose.model('vehicle').schema;
+
+var RaceSchema = new Schema({
+    sections: [{
+        times: [{
+			time: Number
+		}]
+    }],
+    track: TrackSchema,
+    drivers: [DriverSchema],
+    vehicles: [VehicleSchema]
+});
+
+module.exports = mongoose.model('race', RaceSchema);
